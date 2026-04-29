f58d56680869333d87679e4f01d3bf02166d9eb8
仙女 <nicole16312@gmail.com>
2026-04-29 13:30:53 +0800

    Updates

---
+++ assets/_plugin-vue_export-helper-gD74830H.js
@@ -0,0 +1,1 @@
+import{a as e}from"./vue-router-CDlHQlIa.js";var t=e(`user`,{state:()=>({userInfo:null,isLogin:!1}),actions:{setUserInfo(e){this.userInfo=e,this.isLogin=!0},logout(){this.userInfo=null,this.isLogin=!1}}}),n=(e,t)=>{let n=e.__vccOpts||e;for(let[e,r]of t)n[e]=r;return n};export{t as n,n as t};
